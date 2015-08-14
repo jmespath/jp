f18c6ef1a169384ac024ac44c80300661df58279
James Saryerwinnie <js@jamesls.com>
2015-08-13 22:28:37 -0700

    Fix docker build scripts when scripts moved to scripts/

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -3,4 +3,4 @@
 # This script uses docker so you don't have to have the cross
 # platform golang environment setup.  Just make sure you have docker
 # installed.  The built executables will be in build/
-docker run --rm -v "$PWD:/go/src/jp" -w /go/src/jp golang:1.4.2-cross ./build-all-platforms.sh
+docker run --rm -v "$PWD:/go/src/jp" -w /go/src/jp golang:1.4.2-cross scripts/build-all-platforms.sh
