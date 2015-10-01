2f7189792a3730d441480aaacd30be2db49f8095
James Saryerwinnie <js@jamesls.com>
2015-09-30 17:35:07 -0700

    Build arm7 when building all platforms

--- scripts/build-all-platforms.sh
+++ scripts/build-all-platforms.sh
@@ -5,6 +5,13 @@ for goos in darwin linux windows freebsd; do
 	export GOOS="$goos"
 	for goarch in 386 amd64; do
 		export GOARCH="$goarch"
-		go build -v -o build/jp-$GOOS-$GOARCH
+		echo "Building for $GOOS/$GOARCH"
+		go build -v -o build/jp-$GOOS-$GOARCH 2>/dev/null
 	done
 done
+# Also build for ARM7/linux
+export GOOS=linux
+export GOARCH=arm
+export GOARM=7
+echo "Building for $GOOS/$GOARCH/$GOARM"
+go build -v -o build/jp-$GOOS-$GOARCH-arm$GOARM 2> /dev/null
