jmespath/jp

commit e7989df1056486dbf3e1c3b53a2f208a2023efc9

jpp: Add --accumulate, -a option which accumulates all output objects into a single recursively merged object
This option will accumulate all output objects into a single
recursively merged output object. Most of this code comes from the
golang playground link posted by Roger Peppe in this
"Recursively merge JSON structures" thread:

https://groups.google.com/g/golang-nuts/c/nLCy75zMlS8/m/O9ZMubnKCQAJ
https://play.golang.org/p/8jlJUbEJKf

cmd/jpp/main.go
@@ -22,6 +22,10 @@ func main() {
 	app.Email = ""
 	app.Flags = []cli.Flag{
 		cli.BoolFlag{
+			Name:   "accumulate, a",
+			Usage:  "Accumulate all output objects into a single recursively merged output object.",
+		},
+		cli.BoolFlag{
 			Name:   "compact, c",
 			Usage:  "Produce compact JSON output that omits nonessential whitespace.",
 		},
@@ -116,30 +120,59 @@ func runMain(c *cli.Context) int {
 		}
 	}
 
+	var accumulator interface{}
+	eof := false
+
 	for {
-		var input interface{}
-		if c.Bool("slurp") {
-			input = slurpInput
-		} else if err := jsonParser.Decode(&input); err == io.EOF {
-			break
-		} else if err != nil {
-			errMsg("Error parsing input json: %s\n", err)
-			return 2
-		}
-		result, err := jmespath.Search(expression, input)
-		if err != nil {
-			if syntaxError, ok := err.(jmespath.SyntaxError); ok {
-				return errMsg("%s\n%s\n",
-					syntaxError,
-					syntaxError.HighlightLocation())
+		var result interface{}
+		for {
+			var input interface{}
+			var err error
+			if c.Bool("slurp") {
+				input = slurpInput
+			} else if err = jsonParser.Decode(&input); err == io.EOF {
+				eof = true
+				break
+			} else if err != nil {
+				errMsg("Error parsing input json: %s\n", err)
+				return 2
 			}
-			return errMsg("Error evaluating JMESPath expression: %s", err)
+			result, err = jmespath.Search(expression, input)
+			if err != nil {
+				if syntaxError, ok := err.(jmespath.SyntaxError); ok {
+					return errMsg("%s\n%s\n",
+						syntaxError,
+						syntaxError.HighlightLocation())
+				}
+				return errMsg("Error evaluating JMESPath expression: %s", err)
+			}
+
+			if c.Bool("accumulate") {
+				if accumulator == nil {
+					accumulator = result
+				} else {
+					accumulator, err = merge(result, accumulator); if err != nil {
+						errMsg("Error merging output json: %s\n", err)
+						return 2
+					}
+				}
+			} else {
+				break
+			}
 		}
+
+		if c.Bool("accumulate") {
+			result = accumulator
+		} else if eof {
+			break
+		}
+
 		converted, isString := result.(string)
 		if c.Bool("unquoted") && isString {
 			os.Stdout.WriteString(converted)
 		} else {
 			var toJSON []byte
+			var err error
 			if c.Bool("compact") {
 				toJSON, err = json.Marshal(result)
 			} else {
@@ -152,9 +185,82 @@ func runMain(c *cli.Context) int {
 			os.Stdout.Write(toJSON)
 		}
 		os.Stdout.WriteString("\n")
-		if c.Bool("slurp") {
+		if eof || c.Bool("accumulate") || c.Bool("slurp") {
 			break
 		}
 	}
 	return 0
 }
+
+// The following merge and merge1 functions come from the
+// golang playground link posted by Roger Peppe in this
+// "Recursively merge JSON structures" thread:
+//
+// https://groups.google.com/g/golang-nuts/c/nLCy75zMlS8/m/O9ZMubnKCQAJ
+// https://play.golang.org/p/8jlJUbEJKf
+
+// merge merges the two JSON-marshalable values x1 and x2,
+// preferring x1 over x2 except where x1 and x2 are
+// JSON objects, in which case the keys from both objects
+// are included and their values merged recursively.
+//
+// It returns an error if x1 or x2 cannot be JSON-marshaled.
+func merge(x1, x2 interface{}) (interface{}, error) {
+	data1, err := json.Marshal(x1)
+	if err != nil {
+		return nil, err
+	}
+	data2, err := json.Marshal(x2)
+	if err != nil {
+		return nil, err
+	}
+	var j1 interface{}
+	err = json.Unmarshal(data1, &j1)
+	if err != nil {
+		return nil, err
+	}
+	var j2 interface{}
+	err = json.Unmarshal(data2, &j2)
+	if err != nil {
+		return nil, err
+	}
+	return merge1(j1, j2), nil
+}
+
+func merge1(x1, x2 interface{}) interface{} {
+	switch x1 := x1.(type) {
+	case map[string]interface{}:
+		x2, ok := x2.(map[string]interface{})
+		if !ok {
+			return x1
+		}
+		for k, v2 := range x2 {
+			if v1, ok := x1[k]; ok {
+				x1[k] = merge1(v1, v2)
+			} else {
+				x1[k] = v2
+			}
+		}
+	case []interface{}:
+		x2, ok := x2.([]interface{})
+		if !ok {
+			return x1
+		}
+		var result []interface{}
+		for _, element := range x2 {
+			result = append(result, element)
+		}
+		for _, element := range x1 {
+			result = append(result, element)
+		}
+		return result
+	case nil:
+		// merge(nil, map[string]interface{...}) -> map[string]interface{...}
+		x2, ok := x2.(map[string]interface{})
+		if ok {
+			return x2
+		}
+	}
+	return x1
+}
+

test/cases/search.bats
@@ -25,6 +25,18 @@
   [ "$output" == $'\"bar\"\n\"x\"' ]
 }
 
+@test "Test recursive accumulate mode for nested lists" {
+  output=$(echo '{"foo": ["a"]}{"foo": ["x"]}' | ./jpp -a -c @)
+  echo "$output"
+  [ "$output" == '{"foo":["a","x"]}' ]
+}
+
+@test "Test recursive accumulate mode for nested lists" {
+  output=$(echo '["a"]["x"]' | ./jpp -a -c @)
+  echo "$output"
+  [ "$output" == '["a","x"]' ]
+}
+
 @test "Can search subexpr expression" {
   output=$(echo '{"foo": {"bar": "baz"}}' | ./jp foo.bar)
   [ "$output" == "\"baz\"" ]

commit 0abe226af789977087407929fba0e4cc85f9c5a1

jpp: fix --accumulate array merge to coalesce duplicates from different nested lists
Fixes: e7989df10564 ("jpp: Add --accumulate, -a option which accumulates all output objects into a single recursively merged object")

cmd/jpp/main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
+	"reflect"
 
 	"github.com/jmespath/jp/Godeps/_workspace/src/github.com/codegangsta/cli"
 	"github.com/jmespath/jp/Godeps/_workspace/src/github.com/jmespath/go-jmespath"
 )
 
-const version = "0.1.3.1"
+const version = "0.1.3.2"
 
 func main() {
 	app := cli.NewApp()
@@ -251,7 +253,9 @@ func merge1(x1, x2 interface{}) interface{} {
 			result = append(result, element)
 		}
 		for _, element := range x1 {
-			result = append(result, element)
+			if !contains(result, element) {
+				result = append(result, element)
+			}
 		}
 		return result
 	case nil:
@@ -264,3 +268,91 @@ func merge1(x1, x2 interface{}) interface{} {
 	return x1
 }
 
+func equal(lhs interface{}, rhs interface{}) bool {
+	switch lhs := lhs.(type) {
+		case nil:
+			switch rhs.(type) {
+				case nil:
+					return true
+			}
+			return false
+
+		case string:
+			switch rhs := rhs.(type) {
+				case string:
+					if lhs == rhs {
+						return true
+					}
+			}
+			return false
+
+		case int:
+			switch rhs := rhs.(type) {
+				case int:
+					if lhs == rhs {
+						return true
+					}
+			}
+			return false
+
+		case float32:
+			switch rhs := rhs.(type) {
+				case float32:
+					if lhs == rhs {
+						return true
+					}
+			}
+			return false
+
+		case float64:
+			switch rhs := rhs.(type) {
+				case float64:
+					if lhs == rhs {
+						return true
+					}
+			}
+			return false
+		default:
+			panic(fmt.Sprintf("unhandled type comparison: %s vs %s", reflect.TypeOf(lhs), reflect.TypeOf(rhs)))
+	}
+}
+
+func contains(values []interface{}, value interface{}) bool {
+
+	valueData, err := json.Marshal(value); if err != nil {
+		panic(err)
+	}
+
+	for _, v := range values {
+		switch v := v.(type) {
+			case map[string]interface{}:
+				switch value.(type) {
+					case map[string]interface{}:
+						data, err := json.Marshal(v); if err != nil {
+							panic(err)
+						}
+						if bytes.Compare(valueData, data) == 0 {
+							return true
+						}
+				}
+
+			case []interface{}:
+				switch value.(type) {
+					case []interface{}:
+						data, err := json.Marshal(v); if err != nil {
+							panic(err)
+						}
+						if bytes.Compare(valueData, data) == 0 {
+							return true
+						}
+				}
+
+			default:
+				if equal(v, value) {
+					return true
+				}
+		}
+	}
+
+	return false
+}

test/cases/search.bats
@@ -37,6 +37,18 @@
   [ "$output" == '["a","x"]' ]
 }
 
+@test "Test that recursive accumulate mode coalesces duplicates from different nested lists" {
+  output=$(echo '{"foo": ["a", "a"]}{"foo": ["a"]}' | ./jpp -a -c @)
+  echo "$output"
+  [ "$output" == '{"foo":["a","a"]}' ]
+}
+
+@test "Test that recursive accumulate mode preserves duplicates from the same nested list" {
+  output=$(echo '{"foo": ["a", "a"]}{"foo": ["a", "b"]}' | ./jpp -a -c @)
+  echo "$output"
+  [ "$output" == '{"foo":["a","a","b"]}' ]
+}
+
 @test "Can search subexpr expression" {
   output=$(echo '{"foo": {"bar": "baz"}}' | ./jp foo.bar)
   [ "$output" == "\"baz\"" ]